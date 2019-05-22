1b6c6a319aa2d7637c44d2e005673b9db2f22c53
Zach Cossman <Zachcos@gmail.com>
2019-05-22 16:41:19 -0400

    Change incorrect font-face property to correct font-family property

--- src/imports/variables.js
+++ src/imports/variables.js
@@ -6,32 +6,32 @@ const palette = {
 
 const font = {
   sentinel_semibold: {
-    'font-face': "'Sentinel', serif",
+    'font-family': "'Sentinel', serif",
     'font-weight': '600',
     'font-style': 'normal',
   },
   sentinel_bold: {
-    'font-face': "'Sentinel', serif",
+    'font-family': "'Sentinel', serif",
     'font-weight': 'bold',
     'font-style': 'normal',
   },
   circular_book: {
-    'font-face': "'Circular Std Book', sans-serif",
+    'font-family': "'Circular Std Book', sans-serif",
     'font-weight': 'normal',
     'font-style': 'normal',
   },
   circular_book_italic: {
-    'font-face': "'Circular Std Book', sans-serif",
+    'font-family': "'Circular Std Book', sans-serif",
     'font-weight': 'normal',
     'font-style': 'italic',
   },
   circular_black: {
-    'font-face': "'Circular Std', sans-serif",
+    'font-family': "'Circular Std', sans-serif",
     'font-weight': '900',
     'font-style': 'normal',
   },
   circular_black_italic: {
-    'font-face': "'Circular Std', sans-serif",
+    'font-family': "'Circular Std', sans-serif",
     'font-weight': '900',
     'font-style': 'italic',
   },
