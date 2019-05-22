ade3e76110628825ea212bbbc21c4e06c93935c4
Zach Cossman <Zachcos@gmail.com>
2019-05-22 16:21:29 -0400

    Create variables file to hold all design variables

---
+++ src/imports/variables.js
@@ -0,0 +1,40 @@
+const palette = {
+  white: '#ffffff',
+  light: '#c9c9c9',
+  dark: '#35343d',
+};
+
+const font = {
+  sentinel_semibold: {
+    'font-face': "'Sentinel', serif",
+    'font-weight': '600',
+    'font-style': 'normal',
+  },
+  sentinel_bold: {
+    'font-face': "'Sentinel', serif",
+    'font-weight': 'bold',
+    'font-style': 'normal',
+  },
+  circular_book: {
+    'font-face': "'Circular Std Book', sans-serif",
+    'font-weight': 'normal',
+    'font-style': 'normal',
+  },
+  circular_book_italic: {
+    'font-face': "'Circular Std Book', sans-serif",
+    'font-weight': 'normal',
+    'font-style': 'italic',
+  },
+  circular_black: {
+    'font-face': "'Circular Std', sans-serif",
+    'font-weight': '900',
+    'font-style': 'normal',
+  },
+  circular_black_italic: {
+    'font-face': "'Circular Std', sans-serif",
+    'font-weight': '900',
+    'font-style': 'italic',
+  },
+};
+
+export { palette, font };
