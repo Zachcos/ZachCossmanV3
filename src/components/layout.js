7a5a25c0d569408b424ecd7af86aae4bf31d0cc0
Zach Cossman <Zachcos@gmail.com>
2019-05-22 15:39:14 -0400

    Import react helmet and enter helmet information

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,3 +1,13 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 
-export default ({ children }) => <p>hello world from the layout!</p>;
+export default ({ children }) => (
+  <React.Fragment>
+    <Helmet>
+      <meta charset="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
+      <title>ZachCossman.com</title>
+    </Helmet>
+  </React.Fragment>
+);
