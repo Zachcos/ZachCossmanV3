8074ef03c1b8899f86057872260b4ddeb1e94761
Zach Cossman <Zachcos@gmail.com>
2019-05-22 20:42:08 -0400

    Remove unneeded import of layout component on all ppages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import styled from 'styled-components';
-import Layout from '../components/layout';
 import { palette, font } from '../imports/variables';
 
 const Content = styled.div`
