f43ecc1bee3d18878d46bb7eb23f46890320f729
Zach Cossman <Zachcos@gmail.com>
2019-05-22 23:37:40 -0400

    Replace font variable declarations for Google fonts

--- src/imports/variables.js
+++ src/imports/variables.js
@@ -5,34 +5,34 @@ const palette = {
 };
 
 const font = {
-  sentinel_semibold: {
-    'font-family': "'Sentinel', serif",
-    'font-weight': '600',
+  montserrat: {
+    'font-family': "'Montserrat', sans-serif",
+    'font-weight': '500',
     'font-style': 'normal',
   },
-  sentinel_bold: {
-    'font-family': "'Sentinel', serif",
-    'font-weight': 'bold',
-    'font-style': 'normal',
-  },
-  circular_book: {
-    'font-family': "'Circular Std Book', sans-serif",
-    'font-weight': 'normal',
-    'font-style': 'normal',
-  },
-  circular_book_italic: {
-    'font-family': "'Circular Std Book', sans-serif",
-    'font-weight': 'normal',
+  montserrat_italic: {
+    'font-family': "'Montserrat', sans-serif",
+    'font-weight': '500',
     'font-style': 'italic',
   },
-  circular_black: {
-    'font-family': "'Circular Std', sans-serif",
-    'font-weight': '900',
+  montserrat_bold: {
+    'font-family': "'Montserrat', sans-serif",
+    'font-weight': '700',
     'font-style': 'normal',
   },
-  circular_black_italic: {
-    'font-family': "'Circular Std', sans-serif",
-    'font-weight': '900',
+  montserrat_bold_italic: {
+    'font-family': "'Montserrat', sans-serif",
+    'font-weight': '700',
+    'font-style': 'italic',
+  },
+  domine: {
+    'font-family': "'Domine', serif",
+    'font-weight': '700',
+    'font-style': 'normal',
+  },
+  domine_italic: {
+    'font-family': "'Domine', serif",
+    'font-weight': '700',
     'font-style': 'italic',
   },
 };
