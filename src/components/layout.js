c40dc5adffd8aa06e64fa8aa053fd93a1d0b5d28
Zach Cossman <Zachcos@gmail.com>
2019-05-25 10:44:08 -0400

    Add link to fontawesome in helmet

--- src/components/layout.js
+++ src/components/layout.js
@@ -43,6 +43,12 @@ export default ({ children }) => (
         href="https://fonts.googleapis.com/css?family=Domine:400,700|Montserrat:500,500i,700,700i&display=swap"
         rel="stylesheet"
       />
+      <link
+        rel="stylesheet"
+        href="https://use.fontawesome.com/releases/v5.8.2/css/all.css"
+        integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay"
+        crossOrigin="anonymous"
+      />
       <title>ZachCossman.com</title>
     </Helmet>
     <Hero />
