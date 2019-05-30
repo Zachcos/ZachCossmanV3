29ff91e777221bf5fae19b5e6d089636903e6fae
Zach Cossman <Zachcos@gmail.com>
2019-05-29 21:15:15 -0400

    Add resume image and pdf and set up resume page

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -1,45 +1,37 @@
 import React from 'react';
 import styled from 'styled-components';
-import { palette, font } from '../imports/variables';
+import { palette, font, breakpoint } from '../imports/variables';
+
+import resume from '../../static/images/resume.gif';
+import resumePDF from '../../static/images/ZachCossman.pdf';
 
 const Content = styled.div`
-  margin: 30vh 0 0 10vw;
-  .header {
+  margin: 10vh 10vw 0 10vw;
+  img {
+    margin: 0 auto;
+    width: 100%;
+  }
+  @media screen and (${breakpoint}) {
+    margin: 20vh auto 0 auto;
+    width: 95%;
+  }
+`;
+
+const DownloadLink = styled.div`
+  margin-bottom: 50px;
+  text-align: center;
+  a {
     ${font.domine};
     color: ${palette.dark};
-    font-size: 2.75rem;
-    h3 {
-      ${font.domine};
-      color: ${palette.dark};
-      font-size: 2.75rem;
-    }
-    h6 {
-      ${font.montserrat};
-      color: ${palette.dark};
-      font-size: 0.825rem;
-      font-style: italic;
-      letter-spacing: 1px;
-      padding: 5px 0 0 75px;
-    }
-  }
-  .body {
-    margin: 50px;
-      p {
-        ${font.montserrat}
-        color: ${palette.dark};
-        line-height: 1.35rem;
-        width: 40vw;
-      }
+    font-size: 1.35rem;
   }
 `;
 
 export default () => (
   <Content>
-    <div className="header">
-      <h3>work.</h3>
-    </div>
-    <div className="body">
-      <p>This is my resume. This is my test</p>
-    </div>
+    <DownloadLink>
+      <a href={resumePDF}>Download headshot/resume</a>
+    </DownloadLink>
+    <img src={resume} alt="Zach Cossman Resume" />
   </Content>
 );
