a79b2fa45d1325030e8917c5485301286c828315
Zach Cossman <Zachcos@gmail.com>
2019-05-29 21:45:07 -0400

    Additional styles for contact placeholder

--- src/pages/media.js
+++ src/pages/media.js
@@ -10,13 +10,13 @@ const Content = styled.div`
     font-size: 2.75rem;
     h3 {
       ${font.domine};
-      color: ${palette.dark};
+      color: ${font.dark};
       font-size: 2.75rem;
     }
     h6 {
       ${font.montserrat};
       color: ${palette.dark};
-      font-size: 0.825rem;
+      font-size: 0.875rem;
       font-style: italic;
       letter-spacing: 1px;
       padding: 5px 0 0 75px;
@@ -24,12 +24,25 @@ const Content = styled.div`
   }
   .body {
     margin: 50px;
+    p {
+      ${font.montserrat}
+      color: ${palette.dark};
+      line-height: 1.5rem;
+      width: 40vw;
+    }
+    span {
+      ${font.montserrat_bold}
+    }
+  }
+  @media screen and (${breakpoint}) {
+    margin: 25vh auto;
+    width: 80vw;
+    .body {
+      margin: 50px 0;
       p {
-        ${font.montserrat}
-        color: ${palette.dark};
-        line-height: 1.35rem;
-        width: 40vw;
+        width: 100%;
       }
+    }
   }
 `;
 
@@ -39,7 +52,7 @@ export default () => (
       <h3>media.</h3>
     </div>
     <div className="body">
-      <p>This is my media. This is my test</p>
+      <p>media coming soon.</p>
     </div>
   </Content>
 );
