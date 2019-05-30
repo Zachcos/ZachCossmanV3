1ae279f952bab536905f5caa8b7368c5f2ef5ca9
Zach Cossman <Zachcos@gmail.com>
2019-05-29 20:52:58 -0400

    Make style adjustments and add bio copy

--- src/pages/bio.js
+++ src/pages/bio.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import { palette, font } from '../imports/variables';
+import { palette, font, breakpoint } from '../imports/variables';
 
 const Content = styled.div`
   margin: 30vh 0 0 10vw;
@@ -10,13 +10,14 @@ const Content = styled.div`
     font-size: 2.75rem;
     h3 {
       ${font.domine};
-      color: ${palette.dark};
-      font-size: 2.75rem;
+      color: ${font.dark};
+      font-size: 2rem;
+      line-height: 2.35rem;
     }
     h6 {
       ${font.montserrat};
       color: ${palette.dark};
-      font-size: 0.825rem;
+      font-size: 0.875rem;
       font-style: italic;
       letter-spacing: 1px;
       padding: 5px 0 0 75px;
@@ -24,22 +25,59 @@ const Content = styled.div`
   }
   .body {
     margin: 50px;
+    p {
+      ${font.montserrat}
+      color: ${palette.dark};
+      line-height: 1.5rem;
+      width: 40vw;
+    }
+    span {
+      ${font.montserrat_bold}
+    }
+  }
+  @media screen and (${breakpoint}) {
+    margin: 25vh auto;
+    width: 80vw;
+    .body {
+      margin: 50px 0;
       p {
-        ${font.montserrat}
-        color: ${palette.dark};
-        line-height: 1.35rem;
-        width: 40vw;
+        width: 100%;
       }
+    }
   }
 `;
 
 export default () => (
   <Content>
     <div className="header">
-      <h3>all about me.</h3>
+      <h3>
+        Before moving to NYC, Zach spent his time surrounded by music and the
+        arts.
+      </h3>
     </div>
     <div className="body">
-      <p>This is my bio. This is my test</p>
+      <p>
+        Growing up in Fairfield, Ohio, Zach was exposed to music when he began
+        taking drum lessons at the age of 10. His love for music evolved into a
+        love for theatre, which led Zach to a four-year journey at Wright State
+        University in the BFA Musical Theatre program.
+        <br />
+        <br />
+        During his time there, he had the opportunity to be a part of several
+        shows, such as Hairspray, Sweeney Todd, Titanic, RENT, Anything Goes,
+        Little Women, Hello Dolly, and most memorably, Billy Lawlor in 42nd
+        Street, and Leaf Coneybear in The 25th Annual Putnam County Spelling
+        Bee.
+        <br />
+        <br />
+        In the short time following his graduation, Zach had the opportunity to
+        work on the world-premiere production of Revolution in the Elbow of
+        Ragnar Agnarsson Furniture Painter Off-Broadway at the Minetta Lane
+        theatre. Additionally, Zach has worked regionally at theatres such as
+        Cincinnati Playhouse in the Park, Repertory Theatre of St. Louis, Bucks
+        County Playhouse, Theatre Under the Stars, North Carolina Theatre, Casa
+        Mañana, and many others.
+      </p>
     </div>
   </Content>
 );
