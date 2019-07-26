e471b0b52529f901385c0d6ad10384a3af49332d
Zach Cossman <Zachcos@gmail.com>
2019-07-26 01:40:22 -0400

    Add lightGallery plugin files and acheive basic functionality

--- src/pages/media.js
+++ src/pages/media.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import styled from 'styled-components';
+import * as $ from 'jquery';
 import { palette, font, breakpoint } from '../imports/variables';
+import '../css/lightgallery.css';
+import '../js/lightgallery.min.js';
+
+import headshot1 from '../../static/images/headshots/186.jpg';
+import headshot2 from '../../static/images/headshots/141.jpg';
+import headshot3 from '../../static/images/headshots/116.jpg';
 
 const Content = styled.div`
   margin: 30vh 0 0 10vw;
@@ -46,13 +53,37 @@ const Content = styled.div`
   }
 `;
 
-export default () => (
-  <Content>
-    <div className="header">
-      <h3>media.</h3>
-    </div>
-    <div className="body">
-      <p>media coming soon.</p>
-    </div>
-  </Content>
-);
+class Media extends React.Component {
+  componentDidMount() {
+    return (
+      <script>
+        $(document).ready(function() {$('#lightgallery').lightGallery()})
+      </script>
+    );
+  }
+
+  render() {
+    return (
+      <Content>
+        <div className="header">
+          <h3>media.</h3>
+        </div>
+        <div className="body">
+          <div id="lightgallery">
+            <a href={headshot1}>
+              <img src={headshot1} />
+            </a>
+            <a href={headshot2}>
+              <img src={headshot2} />
+            </a>
+            <a href={headshot3}>
+              <img src={headshot3} />
+            </a>
+          </div>
+        </div>
+      </Content>
+    );
+  }
+}
+
+export default Media;
